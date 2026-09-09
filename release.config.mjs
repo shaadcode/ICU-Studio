d1eae16acc87837a09134675f57870b8fc75c839
shaadcode <shaadcode@gmail.com>
2026-09-09 15:04:16 +0330

    chore(release): update release config

--- release.config.mjs
+++ release.config.mjs
@@ -28,10 +28,42 @@ export default {
       '@semantic-release/github',
       {
         assets: [
-          // {
-          //     "path": "src/script.js",
-          //     "label": "script label"
-          // }
+          {
+            label: 'Debian Package (Linux)',
+            // eslint-disable-next-line no-template-curly-in-string
+            name: 'icu-studio-${nextRelease.version}.deb',
+            path: 'src-tauri/target/release/bundle/deb/*.deb',
+          },
+          {
+            label: 'RPM Package (Linux)',
+            // eslint-disable-next-line no-template-curly-in-string
+            name: 'icu-studio-${nextRelease.version}.rpm',
+            path: 'src-tauri/target/release/bundle/rpm/*.rpm',
+          },
+          {
+            label: 'AppImage (Linux)',
+            // eslint-disable-next-line no-template-curly-in-string
+            name: 'icu-studio-${nextRelease.version}.AppImage',
+            path: 'src-tauri/target/release/bundle/appimage/*.AppImage',
+          },
+          {
+            label: 'Windows Installer (NSIS)',
+            path: 'src-tauri/target/release/bundle/nsis/*.exe',
+            // eslint-disable-next-line no-template-curly-in-string
+            name: 'icu-studio-${nextRelease.version}-setup.exe',
+          },
+          {
+            label: 'Windows Installer (MSI)',
+            // eslint-disable-next-line no-template-curly-in-string
+            name: 'icu-studio-${nextRelease.version}.msi',
+            path: 'src-tauri/target/release/bundle/msi/*.msi',
+          },
+          {
+            label: 'macOS Disk Image',
+            // eslint-disable-next-line no-template-curly-in-string
+            name: 'icu-studio-${nextRelease.version}.dmg',
+            path: 'src-tauri/target/release/bundle/dmg/*.dmg',
+          },
         ],
       },
     ],
